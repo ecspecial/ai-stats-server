3c1bfc0f505a90a04925381f8c66c649561c8f85
ecspecial <graffitiraksar55@gmail.com>
2024-06-13 12:50:48 +0300

    Test update for caching

---
+++ app/api/middleware.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+const allowedOrigins = [
+    'http://localhost:3001', 
+    'https://mai-admin.com',
+    'https://www.mai-admin.com',
+  ];
+
+  export function middleware(req: NextRequest) {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // retrieve the HTTP "Origin" header 
+    // from the incoming request
+    req.headers.get("origin")
+
+    // Set no-cache headers
+    res.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.headers.set('Pragma', 'no-cache');
+    res.headers.set('Expires', '0');
+
+    // if the origin is an allowed one,
+    // add it to the 'Access-Control-Allow-Origin' header
+    if (allowedOrigins.includes(origin)) {
+      res.headers.append('Access-Control-Allow-Origin', origin);
+    }
+
+    // add the remaining CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+    matcher: '/api/:path*',
+}
